e4ebe29048173199b5e628f3f5b43ee1ff2b364b
YananWang <1019707077@qq.com>
2016-09-01 14:22:46 +0800

    11

--- myTest/animation/loadImg.js
+++ myTest/animation/loadImg.js
@@ -4,7 +4,7 @@
 function loadImg(arr,callback){
     var result={};
     var iNow=0;
-    var per = document.getElementById("percent");
+    var per = $("#percents");
     console.log(per);
     for(var i=0;i<arr.length;i++){
         var oImg=new Image();
@@ -12,9 +12,11 @@ function loadImg(arr,callback){
         result[tmp[0]]=oImg;
         oImg.onload=function(){
             iNow++;
-            var nowPercent=iNow/arr.length*100;
-            per.innerHTML=nowPercent+"%";
-            //console.log(nowPercent);
+            var nowPercent=parseInt(iNow/arr.length*100);
+            $("#colors").animate({width:nowPercent+'%'},100,function(){
+                $("#percents").text(nowPercent+"%");
+            });
+            console.log(nowPercent);
             if(iNow>=arr.length){
                 callback(result);
             }
@@ -22,6 +24,18 @@ function loadImg(arr,callback){
         oImg.src=arr[i];
     }
 }
-loadImg(['boy.png','bg1.png','bg2.png','bg3.png'],function(result){
+loadImg(['boy.png','bg1.png','bg2.png',
+         'bg3.png',
+         'http://photocdn.sohu.com/20160825/Img465865813.jpg',
+         'http://img.daimg.com/uploads/allimg/160805/1-160P5002417.jpg',
+         'http://bpic.588ku.com/back_pic/00/01/88/75/cdc49f31dbda8b9eac3b481d04542cce.jpg!qianku1198'],function(result){
     console.log(result)
 });
+$(function(){
+    $("#my_btn").click(function(){
+        loadImg(['boy.png','bg1.png','bg2.png','bg3.png'],function(result){
+            console.log(result)
+        });
+    });
+});
+
