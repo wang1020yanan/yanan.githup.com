b57acb1fed964cfb01fec13be96c9fe2c2422de7
YananWang <1019707077@qq.com>
2016-10-20 18:11:20 +0800

    1

--- myTest/game1/main.js
+++ myTest/game1/main.js
@@ -1,6 +1,6 @@
 cc.game.onStart = function(){
     cc.view.adjustViewPort(true);
-    cc.view.setDesignResolutionSize(720, 1280, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.setDesignResolutionSize(800, 1280, cc.ResolutionPolicy.SHOW_ALL);
     cc.view.resizeWithBrowserSize(true);
     //load resources
 
